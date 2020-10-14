a2c9e34f7a756f1cdef12378340afab82e866e0c
Maram-Emad <69482930+Maram-Emad@users.noreply.github.com>
2020-10-15 00:56:24 +0300

    script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+var names = ["John", "Jen", "Jason","Jim"];
+
+for (var i = 0; i < names.length; i++) {
+  console.log("Good" + "bye" + " " + names[i]);
+}
+
+var names = ["Frank", "Larry", "Paula","Laura", "Paul", "Yaakov"];
+
+for (var i = 0; i < names.length; i++) {
+  console.log("Hello" + " " + names[i]);
+}
